c66d1ead2a3699a28bcf6ab4a6a6701e63f18121
krishanth kumar <84780743+krishanth31875@users.noreply.github.com>
2022-11-07 18:52:38 +0530

    Deployed SA for AR

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import pickle
 import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import sequence
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def main():
+    maxlen=100
+    st.title('Alexa Sentiment Classifier')
+    review_description = st.text_area('Review Description:')
+    prediction  = st.button('Sentiment Prediction')
+    with open('tokenizing_text.pkl', 'rb') as fid:
+        tokenizer = pickle.load(fid)
+    sentiment_model = load_model('customize_conv_networke.h5')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    if prediction and review_description != '':
+        description = [review_description]
+        pred = sentiment_model.predict(sequence.pad_sequences(tokenizer.texts_to_sequences(description),maxlen=maxlen))  
+        if pred>0.5:
+            st.success(f'The sentiment is positive')
+        else:
+            st.error(f'The sentiment is negative')
+    elif prediction and review_description == '':
+        st.warning('Please enter your review')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__=='__main__':
+    main()
